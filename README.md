0954199381da9cf4eb49c129ddc875578af02de3
Karol Maj <karol.maj@schibsted.pl>
2017-08-10 17:18:16 +0200

    Initial version

---
+++ README.md
@@ -0,0 +1,9 @@
+# Hapi Enforce HTTPS Plugin [![Build Status](https://travis-ci.org/plan3/hapi-enforce-https.svg?branch=master)](https://travis-ci.org/plan3/hapi-enforce-https)
+
+## Installtion
+
+`npm install hapi-enforce-https`
+
+## Usage
+
+
---
+++ index.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const boom = require('boom');
+
+/**
+ * @typedef {Object} Options
+ * @property {Boolean} enforceHttps
+ */
+
+/**
+ * @param {Object} server
+ * @param {Options} options
+ * @param {Function} next
+ * @returns {Function}
+ */
+module.exports.register = function(server, options, next) {
+    if (options.enforceHttps) {
+        server.ext('onRequest', function(request, reply) {
+            if (request.connection.info.protocol !== 'https'
+                && request.raw.req.headers['x-forwarded-proto'] !== 'https') {
+                return reply(boom.badRequest('HTTPS required'));
+            }
+
+            return reply.continue();
+        });
+    }
+    return next();
+};
+
+module.exports.register.attributes = {
+    pkg: require('./package.json')
+};
